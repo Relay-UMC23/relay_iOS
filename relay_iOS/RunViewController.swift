31af633363a0ee8b12e3bf5b61eae0a3a23a652c
realisshomyang <realisshoman@kakao.com>
2023-01-23 18:41:05 +0900

    Design : StoryBoard 구성 변경
    스토리보드 추가 : run 스토리보드 추가

---
+++ relay_iOS/RunViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RunViewController.swift
+//  relay_iOS
+//
+//  Created by 조명근 on 2023/01/23.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class RunViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()// location2
+
+    }
+    
+    
+}
