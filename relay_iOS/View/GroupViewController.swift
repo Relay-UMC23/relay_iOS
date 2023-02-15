0f582c90e91cb53793f02d30897556ba51fd08f4
jinyoung <jinqueen1@naver.com>
2023-02-15 14:47:19 +0900

    feat : 야옹 ㅎ그룹 화면 추가

--- relay_iOS/View/GroupViewController.swift
+++ relay_iOS/View/GroupViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 class GroupViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-
-
+    
+    
+    @IBAction func back(_ sender: UIButton) {
+    _ = self.navigationController?.popViewController(animated: true) }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memoName.count
@@ -30,25 +32,20 @@ class GroupViewController: UIViewController,UITableViewDataSource,UITableViewDel
     
     
     override func viewDidLoad() {
-     super.viewDidLoad()
+        super.viewDidLoad()
     }
     
 }
 
-
-class CustomCell : UITableViewCell {
-    @IBOutlet weak var labelName: UILabel!
-    @IBOutlet weak var labelContent: UILabel!
-    @IBOutlet weak var UserProfile:UIImageView!
     
-    
-
-
-}
-let memoName = ["리페", "야옹","혜콩","테오","설기","비카","샐리","솜"]
-let memoContent = ["저는 방장 리페입니다","달리기를 좋아하는 야옹이에요", "안녕하세요 혜콩입니다","달리기를 잘하는 테오입니다", "저는 운동을 좋아합니다", "저는 항상 학교까지 달려간답니다","먹는것보다 달리는게 좋아요", " 운동중에 달리기를 제일 좋아해요"]
-
-
-
-
-
+    class CustomCell : UITableViewCell {
+        @IBOutlet weak var labelName: UILabel!
+        @IBOutlet weak var labelContent: UILabel!
+        @IBOutlet weak var UserProfile:UIImageView!
+        
+        
+        
+        
+    }
+    let memoName = ["리페", "야옹","혜콩","테오","설기","비카","샐리","솜"]
+    let memoContent = ["저는 방장 리페입니다","달리기를 좋아하는 야옹이에요", "안녕하세요 혜콩입니다","달리기를 잘하는 테오입니다", "저는 운동을 좋아합니다", "저는 항상 학교까지 달려간답니다","먹는것보다 달리는게 좋아요", " 운동중에 달리기를 제일 좋아해요"]
--- relay_iOS/View/PersonalViewController.swift
+++ relay_iOS/View/PersonalViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PersonalViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
